e9793edb5c32eba3ba9da8cbdf41b3059802a986
Vytautas Šablinskas <vytsab2@ktu.lt>
2023-12-24 22:24:44 +0200

    Remove API which was only for test purposes

--- TheBlog.API/Dtos/Authentication/UserDto.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-namespace TheBlog.API.Dtos.Authentication
-{
-    [ExcludeFromCodeCoverage]
-    public record UserDto(string Id, string UserName, string Email);
-}
